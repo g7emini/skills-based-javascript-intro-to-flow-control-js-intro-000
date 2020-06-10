0c3c7e851f334e38307e7bd833feba96d5aeda05
groana <contact@groanamelendez.com>
2020-06-10 17:03:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,24 +5,24 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if(age >= 13 && age <= 19) {
-    return "You are a teenager!"
+    return "You are a teenager!";
   } else {
-    return "You are not a teenager"
+    return "You are not a teenager";
   }
 }
 
 function ageChecker(age) {
   if(age >= 13 && age <= 19) {
-  return "You are a teenager!"
+  return "You are a teenager!";
   } else if(age <= 12) {
-    return "You are a kid"
+    return "You are a kid";
   } else {
-    return "You are a grownup"
+    return "You are a grownup";
   }
 }
 
 function ternaryTeenager(age) {
-  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
+  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
@@ -31,7 +31,7 @@ function switchAge(age) {
        console.log("You are a teenager");
        break;
     default : 
-      return "You have an age" 
+      return "You have an age"; 
   }
-  console.log(13, 14, 15, 16, 17, 18, 19)
+  console.log(13, 14, 15, 16, 17, 18, 19);
 }
