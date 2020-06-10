764b68bacaad3c060711f07f2c817ad51d4a0e2e
groana <contact@groanamelendez.com>
2020-06-10 17:18:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,8 +28,8 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch(age) {
     case 13:
-      
-    
+      return "You are a teenager";
+    default:
       return "You have an age"; 
   }
 }
