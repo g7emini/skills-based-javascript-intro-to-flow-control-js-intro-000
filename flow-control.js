1a2ea75dd6538f5fce3a6de0ce24cdf305db99ae
groana <contact@groanamelendez.com>
2020-06-10 17:05:05 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,7 +27,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case 13>=age<=19:
+    case age >= 13 && age <= 19:
        console.log("You are a teenager");
        break;
     default : 
